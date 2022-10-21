0d78e3366ca2246d0395e528d43a8be4dd8f298d
Juan Duque <jduque@princeton.edu>
2022-10-21 14:44:46 -0400

    Debug CoinGame, add rendering

--- src/coin_game.py
+++ src/coin_game.py
@@ -18,12 +18,12 @@ AGENT_2 = 4
 BOTH = 5
 BOTH_AND_COIN = 6
 
-BLOCKSIZE = 20
+BLOCKSIZE = 100
 
 class CoinGame(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, size_h, size_v, default=True, positions=None):
+    def __init__(self, size_h, size_v, default=True, positions=None, render_mode=None):
         if size_h < 2 or size_v < 1:
             raise Exception("Invalid world size")
         
@@ -38,6 +38,11 @@ class CoinGame(gym.Env):
 
         self.screen_width = size_h * BLOCKSIZE
         self.screen_height = size_v * BLOCKSIZE
+
+        self.screen = None
+        self.clock = None
+
+        self.render_mode = render_mode
 
     def init_grid(self, positions=None):
         if positions:
@@ -61,15 +66,22 @@ class CoinGame(gym.Env):
         return positions
 
     def step(self, actions):
+        terminated = False
         a1, a2 = actions
         a_1_x, a_1_y = self.a_1//self.cols, self.a_1%self.cols
         a_2_x, a_2_y = self.a_2//self.cols, self.a_2%self.cols
 
-        self.grid[a_1_y, a_1_x] = EMPTY
-        self.grid[a_2_y, a_2_x] = EMPTY
+        print("A1: ", a_1_x, " ", a_1_y)
+        print("A2: ", a_2_x, " ", a_2_y)
+
+        self.grid[a_1_x, a_1_y] = EMPTY
+        self.grid[a_2_x, a_2_y] = EMPTY
 
         new_a_1_y, new_a_1_x = self.calculate_pos(a_1_x, a_1_y, a1)
         new_a_2_y, new_a_2_x = self.calculate_pos(a_2_x, a_2_y, a2)
+
+        print("New A1: ", new_a_1_x, " ", new_a_1_y)
+        print("New A2: ", new_a_2_x, " ", new_a_2_y)
 
         self.a_1 = new_a_1_y*self.cols + new_a_1_x
         self.a_2 = new_a_2_y*self.cols + new_a_2_x
@@ -103,15 +115,15 @@ class CoinGame(gym.Env):
 
         return self.grid, reward, terminated, False, {}
 
-    def calculate_pos(self, a_x, a_y, a):
+    def calculate_pos(self, a_y, a_x, a):
         if a==UP:
             a_y = max(0, a_y-1)
         elif a==DOWN:
-            a_y = min(0, a_y+1)
+            a_y = min(self.rows-1, a_y+1)
         elif a==LEFT:
-            a_x = min(0, a_x+1)
+            a_x = max(0, a_x-1)
         elif a==RIGHT:
-            a_x = min(0, a_x+1)
+            a_x = min(self.cols-1, a_x+1)
         return (a_y, a_x)
 
 
@@ -147,10 +159,45 @@ class CoinGame(gym.Env):
             self.clock = pygame.time.Clock()
 
         self.surf = pygame.Surface((self.screen_width, self.screen_height))
-        self.surf.fill((0, 0, 0))
+        self.surf.fill((255, 255, 255))
 
         for x in range(0, self.screen_width, BLOCKSIZE):
             for y in range(0, self.screen_height, BLOCKSIZE):
                 rect = pygame.Rect(x, y, BLOCKSIZE, BLOCKSIZE)
-                pygame.draw.rect(self.screen, (255, 255, 255), rect, 1)
-        
+                pygame.draw.rect(self.surf, (0, 0, 0), rect, 1)
+
+        for i, x in enumerate(range(0, self.screen_width, BLOCKSIZE)):
+            for j, y in enumerate(range(0, self.screen_height, BLOCKSIZE)):
+                if self.grid[j, i]==BOTH or self.grid[i, j]==BOTH_AND_COIN:
+                    rect_a1 = pygame.Rect(x+2, y+2, BLOCKSIZE-4, BLOCKSIZE//2 - 2)
+                    rect_a2 = pygame.Rect(x+2, y + BLOCKSIZE//2, BLOCKSIZE-4, BLOCKSIZE//2 - 2)
+                    pygame.draw.rect(self.surf, (255, 0, 0), rect_a1, 0)
+                    pygame.draw.rect(self.surf, (0, 0, 255), rect_a2, 0)
+                if self.grid[j, i]==COIN_1:
+                    pos = (x+BLOCKSIZE//2, y+BLOCKSIZE//2)
+                    pygame.draw.circle(self.surf, (255,0,0), pos, BLOCKSIZE//2 - 4, 0)
+                    pygame.draw.circle(self.surf, (139,0,0), pos, BLOCKSIZE//2 - 4, 4)
+                if self.grid[j, i]==COIN_2:
+                    pos = (x+BLOCKSIZE//2, y+BLOCKSIZE//2)
+                    pygame.draw.circle(self.surf, (0,0,255), pos, BLOCKSIZE//2 - 4, 0)
+                    pygame.draw.circle(self.surf, (0,0,139), pos, BLOCKSIZE//2 - 4, 4)
+                if self.grid[j, i]==AGENT_1:
+                    rect_a1 = pygame.Rect(x+2, y+2, BLOCKSIZE-4, BLOCKSIZE//2 - 2)
+                    pygame.draw.rect(self.surf, (255, 0, 0), rect_a1, 0)
+                if self.grid[j, i]==AGENT_2:
+                    rect_a2 = pygame.Rect(x+2, y + BLOCKSIZE//2, BLOCKSIZE-4, BLOCKSIZE//2 - 2)
+                    pygame.draw.rect(self.surf, (0, 0, 255), rect_a2, 0)
+                
+
+        self.surf = pygame.transform.flip(self.surf, False, True)
+        self.screen.blit(self.surf, (0, 0))
+
+        if self.render_mode == "human":
+            pygame.event.pump()
+            self.clock.tick(30)
+            pygame.display.flip()
+
+        elif self.render_mode == "rgb_array":
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(self.screen)), axes=(1, 0, 2)
+            )

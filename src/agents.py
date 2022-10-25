3af006783acb8a8192ebd4734d917704a68416f5
Juan Duque <jduque@princeton.edu>
2022-10-25 12:53:03 -0400

    Added models.py

---
+++ src/agents.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+class NashDQNAgent():
+
+    def __init__(self):
+        pass
+
+    def target(self):
+        """Returns the target Q-values for states/observations."""
+        pass
+
+    def step(self):
+        """Compute the discrete distribution for the Q-value for each
+        action for each state/observation (no grad)."""
+        pass

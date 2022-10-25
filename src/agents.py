bc340a4bb915ca5dd0ecfe8d808a931d24c253fe
Juan Duque <jduque@princeton.edu>
2022-10-25 15:50:59 -0400

    Add config.yaml

--- src/agents.py
+++ src/agents.py
@@ -2,7 +2,21 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
-class NashACAgent():
+class DQNAgent():
+
+    def __init__(self):
+        pass
+
+    def target(self):
+        """Returns the target Q-values for states/observations."""
+        pass
+
+    def step(self):
+        """Compute the discrete distribution for the Q-value for each
+        action for each state/observation (no grad)."""
+        pass
+
+class NashDQNAgent():
 
     def __init__(self):
         pass
